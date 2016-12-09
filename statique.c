2ffc8cf896f2eb8e1bd53491601ec52980b262c8
florianfritsch <florian.fritsch@viacesi.fr>
2016-12-09 14:14:12 +0100

    Add files via upload

--- statique.c
+++ statique.c
@@ -1,37 +1,74 @@
-#include<stdio.h> 
-#include<stdlib.h>  
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
+
 {
-    
+
 FILE* fichier = NULL;
+
 int caractereActuel = 0;
- 
-fichier = fopen("1.pbm", "r");
- 
+    
+int compteurLigne = 0;
+
+int nombreAleatoire = 1;
 
 
-        // Boucle de lecture des caractères un à un
-do
-	{
-	caractereActuel = fgetc(fichier); // On lit le caractère
-           // printf("%c", caractereActuel); // On l'affiche
-	if (caractereActuel == 48)// 0
-	{
-	printf("X");
+
+if(nombreAleatoire==1)
+	{	
+	fichier = fopen("statique1.pbm", "r");
+	}
+if(nombreAleatoire==2)
+	{	
+	fichier = fopen("statique2.pbm", "r");
+	}
+if(nombreAleatoire==3)
+	{	
+	fichier = fopen("statique3.pbm", "r");
 	}
 
-	if (caractereActuel == 49)// 1
+
+
+	if (fichier != NULL)
+
 	{
-	printf(" ");
+
+        // Boucle de lecture des caracteres un a un
+
+        do
+		
+
+        {
+
+		caractereActuel = fgetc(fichier); // On lit le caractere
+
+	switch(caractereActuel)
+{
+case 49: //1
+	printf("%c",178);
+break;
+case 48: //0
+	printf("%c",176);
+break;
+case '\t': //tab
+	//printf("er");
+break;
+default:
+	printf("\n");
+	compteurLigne++;
+break;
 }
 
+	
 
-        } 
-while (caractereActuel != EOF); // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
- 
-        fclose(fichier);
 
- 
-    return 0;
+        } while (compteurLigne < 24 ); // Tant que le compteurLigne est inférieur à 24, la lecture et l'ecriture de l'image continue
+
+	fclose(fichier);
+
+}
+
+return 0;
 }
