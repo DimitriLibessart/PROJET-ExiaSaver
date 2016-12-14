9a11edb85c5690c7449a4204bf224de8f1eec847
florianfritsch <florian.fritsch@viacesi.fr>
2016-12-14 19:37:39 +0100

    Add files via upload

--- boucle.c
+++ boucle.c
@@ -9,26 +9,72 @@ int TimerPointActualisation=0; // initialiser TimerPointActualisation pour comme
 int caractereActuel = 0;
 int compteurLigne = 0;
 
-FILE* date = NULL; //pointer "date" en tant qu'un fichier
+FILE* fichier = NULL; //pointer "date" en tant qu'un fichier
+FILE* date = NULL;
 
-system("clear"); // demande au système de nettoyer la console
-
-date = fopen("/home/florian/Documents/modeVeille/date.txt", "r+");
+fichier = fopen("/home/florian/Documents/modeVeille/date.txt", "w");
 printf("\n");// retour à la ligne
 system("date +%T");
 
-if (date !=NULL)
+if (fichier !=NULL)
         {
         time_t now= time (NULL);
         struct tm tm_now = *localtime (&now);
         char s_now[sizeof "HH:MM:SS"];
         strftime (s_now, sizeof s_now, "%H:%M:%S", &tm_now);
-        fprintf (date,"%s", s_now);
+        fprintf (fichier,"%s", s_now);
 	}
 
-fclose(date);
+fclose(fichier);
+
+
+	fichier = fopen("/home/florian/Documents/modeVeille/date.txt", "r");
+
+
+
+
+	if (fichier != NULL)
+
+	{
+
+        // Boucle de lecture des caracteres un a un
+
+        do
+		
+
+        {
+
+		caractereActuel = fgetc(fichier); // On lit le caractere
+
+	switch(caractereActuel)
+{
+case 49: //1
+	printf("%c",178);
+break;
+case 48: //0
+	printf("%c",176);
+break;
+default:
+	printf("\n");
+	compteurLigne++;
+break;
+}
+
+	
+
+
+        } while (compteurLigne < 8 ); // Tant que le compteurLigne est inférieur à 24, la lecture et l'ecriture de l'image continue
+
+	fclose(fichier);
+
+	
+}
+	
+
 
 printf("\nCet ecran sera actualise dans quelques secondes ");// afficher le message d'actualisation
+
+//============================================DEBUT DE LA BOUCLE==========================================================================
 
 while(TimerPointActualisation < 100)// boucle infini
 	{
