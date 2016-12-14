b8598c0ed4a2685c166ed9bc049eb4b45d31ad7b
DimitriLibessart <dimitri.libessart@viacesi.fr>
2016-12-14 09:21:31 +0100

    STATIQUE FINAL

--- statique.c
+++ statique.c
@@ -1,46 +1,50 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
 
 int main(int argc, char *argv[])
 
 {
 
-system("clear");  //on nettoie le terminal de toute commande
+system("clear");
 
-FILE* fichier = NULL; //initialisation du fichier à "NULL"
+FILE* fichier = NULL;
 
-int caractereActuel = 0; //initialisation de la variable caractéreActuel
-     
-int compteurLigne = 0;  //initialisation de la variable compteurLigne
+int caractereActuel = 0;
+    
+int compteurLigne = 0;
 
-int Aleatoire (int _iMin, int _iMax)
+int aAleatoire (int _iMin, int _iMax)
 {
 return (_iMin + (rand () % (_iMax-_iMin+1)));
 }
 
-int nombreAleatoire;   //choix aléatoire d'un numéro d'image 
-
+int nombreAleatoire;
+sleep(1);
 srand (time (NULL));
-nombreAleatoire = Aleatoire (1,3);
+nombreAleatoire = aAleatoire (4,6);
 
-if(nombreAleatoire==1)
+if(nombreAleatoire==4)
 	{
 
-	fichier = fopen("statique1couleur.pbm", "r");
+	fichier = fopen("statique1.pbm", "r");
 	}
-if(nombreAleatoire==2)
+if(nombreAleatoire==5)
 	{	
 
-	fichier = fopen("statique2couleur.pbm", "r");
+	fichier = fopen("statique2.pbm", "r");
 	}
-if(nombreAleatoire==3)
+if(nombreAleatoire==6)
 	{
 
-	fichier = fopen("statique3couleur.pbm", "r");
+	fichier = fopen("statique3.pbm", "r");
 	}
-
-
 
 	if (fichier != NULL)
 
@@ -55,7 +59,7 @@ if(nombreAleatoire==3)
 
 		caractereActuel = fgetc(fichier); // On lit le caractere
 
-	switch(caractereActuel)  //fonction pour remplacer les valeurs binaires en caractére de la table ASCII
+	switch(caractereActuel)
 {
 case 49: //1
 	printf("%c",178);
@@ -63,9 +67,7 @@ break;
 case 48: //0
 	printf("%c",176);
 break;
-case '\t': //tab
-	//printf("er");
-break;
+
 default:
 	printf("\n");
 	compteurLigne++;
@@ -75,11 +77,11 @@ break;
 	
 
 
-        } while (compteurLigne < 24 ); // Tant que le compteurLigne est inférieur à 24, la lecture et l'ecriture de l'image continue
+    } while (compteurLigne < 24 ); // Tant que le compteurLigne est inférieur à 24, la lecture et l'ecriture de l'image continue
 
 	fclose(fichier);
-}  //fonction de sortie pour rendre la main au shell
-printf("Appuyez sur une touche pour quitter le mode veille");
+}
+//printf("Appuyez sur une touche pour quitter le mode veille");
 system("stty cbreak -echo");
 getchar();
 system("stty cooked echo");
