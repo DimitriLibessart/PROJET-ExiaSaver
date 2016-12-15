67e019232bf07676a1b6610d8d0ee45f4547a674
florianfritsch <florian.fritsch@viacesi.fr>
2016-12-15 11:45:46 +0100

    Update dynamique.c

--- Modes Veille/dynamique.c
+++ Modes Veille/dynamique.c
@@ -6,11 +6,10 @@
 int main(int argc,char *argv[])
 {
 int TimerPointActualisation=0; // initialiser TimerPointActualisation pour commencer la boucle des points et de l'acutalisation
-int caractereActuel = 0;// initialiser caractereActuel à 0 pour la lecture de pbm
+int caractereActuel = 0;// initialiser caractereActuel à 0 pour la lecture du fichier "date"
 int compteurLigne = 0;// initialiser compteurLigne à 0 pour la lecture de pbm
 
 FILE* fichier = NULL; //pointer "fichier" en tant qu'un fichier
-FILE* date = NULL;// pointer "date" en tant qu'un fichier
 
 fichier = fopen("/home/florian/Documents/modeVeille/date.txt", "w");// ouverture du fichier "date" ou nous allons stocker l'heure en écriture
 printf("\n");// retour à la ligne
@@ -41,23 +40,50 @@ fclose(fichier);// fermeture du fichier
 
 	{
 
-	caractereActuel = fgetc(fichier); // lecture de chaque caractère dans le fichier
+	caractereActuel = fgetc(fichier); // lecture de chaque caractère dans le fichier date
 
 	switch(caractereActuel)
 {
-case 49: //si on rencontre 1
-	printf("%c",178);//on affiche un carré blanc
-break;// sortir de la boucle de rencontre 1
 case 48: //si on rencontre 0
-	printf("%c",176);//on affiche un carré gris
+	printf("\nAffichage du pbm 0");
 break;// sortir de la boucle de rencontre 0
+case 49: //si on rencontre 1
+	printf("\nAffichage du pbm 1");
+break;// sortir de la boucle de rencontre 1
+case 50: //si on rencontre 2
+	printf("\nAffichage du pbm 2");
+break;// sortir de la boucle de rencontre 2
+case 51: //si on rencontre 3
+	printf("\nAffichage du pbm 3");
+break;// sortir de la boucle de rencontre 3
+case 52: //si on rencontre 4
+	printf("\nAffichage du pbm 4");
+break;// sortir de la boucle de rencontre 4
+case 53: //si on rencontre 5
+	printf("\nAffichage du pbm 5");
+break;// sortir de la boucle de rencontre 5
+case 54: //si on rencontre 6
+	printf("\nAffichage du pbm 6");
+break;// sortir de la boucle de rencontre 6
+case 55: //si on rencontre 7
+	printf("\nAffichage du pbm 7");
+break;// sortir de la boucle de rencontre 7
+case 56: //si on rencontre 8
+	printf("\nAffichage du pbm 8");
+break;// sortir de la boucle de rencontre 8
+case 57: //si on rencontre 9
+	printf("\nAffichage du pbm ");
+break;// sortir de la boucle de rencontre 9
+case 58: //si on rencontre :
+	printf("\nAffichage du pbm pour afficher les deux points");
+break;// sortir de la boucle de rencontre :
 default:
 	printf("\n");//retour à la ligne quand il n'y a plus de caractères
 	compteurLigne++;//on ajoute 1 au compteurLigne 
 break;// sortir de la boucle de rencontre de fin de ligne
 }
 
-        } while (compteurLigne < 8 ); // Tant que le compteurLigne est inférieur à 8, la lecture et l'ecriture de l'image continue
+        } while (compteurLigne < 2 ); // Tant que le compteurLigne est inférieur à 8, la lecture et l'ecriture de l'image continue
 
 	fclose(fichier);// fermeture du fichier "date"
 
@@ -117,19 +143,46 @@ if (fichier !=NULL)
 
 	switch(caractereActuel)
 {
-case 49: //si on rencontre 1
-	printf("%c",178);//on affiche un carré blanc
-break;// sortir de la boucle de rencontre 1
 case 48: //si on rencontre 0
-	printf("%c",176);//on affiche un carré gris
+	printf("\nAffichage du pbm 0");
 break;// sortir de la boucle de rencontre 0
+case 49: //si on rencontre 1
+	printf("\nAffichage du pbm 1");
+break;// sortir de la boucle de rencontre 1
+case 50: //si on rencontre 2
+	printf("\nAffichage du pbm 2");
+break;// sortir de la boucle de rencontre 2
+case 51: //si on rencontre 3
+	printf("\nAffichage du pbm 3");
+break;// sortir de la boucle de rencontre 3
+case 52: //si on rencontre 4
+	printf("\nAffichage du pbm 4");
+break;// sortir de la boucle de rencontre 4
+case 53: //si on rencontre 5
+	printf("\nAffichage du pbm 5");
+break;// sortir de la boucle de rencontre 5
+case 54: //si on rencontre 6
+	printf("\nAffichage du pbm 6");
+break;// sortir de la boucle de rencontre 6
+case 55: //si on rencontre 7
+	printf("\nAffichage du pbm 7");
+break;// sortir de la boucle de rencontre 7
+case 56: //si on rencontre 8
+	printf("\nAffichage du pbm 8");
+break;// sortir de la boucle de rencontre 8
+case 57: //si on rencontre 9
+	printf("\nAffichage du pbm ");
+break;// sortir de la boucle de rencontre 9
+case 58: //si on rencontre :
+	printf("\nAffichage du pbm pour afficher les deux points");
+break;// sortir de la boucle de rencontre :
 default:
 	printf("\n");//retour à la ligne quand il n'y a plus de caractères
 	compteurLigne++;//on ajoute 1 au compteurLigne 
 break;// sortir de la boucle de rencontre de fin de ligne
 }
 
-        } while (compteurLigne < 8 ); // Tant que le compteurLigne est inférieur à 8, la lecture et l'ecriture de l'image continue
+        } while (compteurLigne < 2 ); // Tant que le compteurLigne est inférieur à 8, la lecture et l'ecriture de l'image continue
 
 	fclose(fichier);// fermeture du fichier "date"
 	}
